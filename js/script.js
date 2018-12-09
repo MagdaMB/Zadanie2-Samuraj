2c9201cb8c5158a61511c60c5bb370d88cc7a003
MagdaMB <magda.m.badura@gmail.com>
2018-12-09 21:06:52 +0100

    extra task

--- js/script.js
+++ js/script.js
@@ -1,29 +1,32 @@
-const box = document.createElement("div");
-box.classList.add("sqaure");
-document.body.appendChild(box);
+document.body.style.height = 10000 + 'px';
+let size = 100;
+let changeRed = true;
 
-let size = 50;
-box.style.width = size + "px";
-box.style.height = size + "px";
-let change = true; // flaga
+const line = document.createElement('div');
+document.body.appendChild(line);
 
-const scrollBox = () => {
+line.style.position = "fixed";
+line.style.width = 100 + '%';
+line.style.backgroundColor = 'green';
+line.style.top = 0;
+line.style.left = 0;
+line.style.height = size + 'px';
 
-    if (size >= window.innerWidth / 2) {
-        change = !change;
+const changeColorAndHeight = () => {
+    if (size > window.innerHeight / 2) {
+        changeRed = !changeRed; //false
+        line.style.backgroundColor = 'red';
     } else if (size <= 0) {
-        change = !change;
+        changeRed = !changeRed; //true
+        line.style.backgroundColor = 'green';
     }
 
-    if (change) {
-        size++;
-        box.style.width = size + "px";
-        box.style.height = size + "px";
+    if (changeRed) {
+        size += 5;
+        line.style.height = size + 'px';
     } else {
-        size--;
-        box.style.width = size + "px";
-        box.style.height = size + "px";
+        size -= 5;
+        line.style.height = size + 'px';
     }
 }
-
-window.addEventListener('scroll', scrollBox);
+window.addEventListener('scroll', changeColorAndHeight);
